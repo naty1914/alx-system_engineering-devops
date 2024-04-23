0abc864c7eabefcca268cb98876f7305e9eadce5
Natnael Seifu <natiseifu02@gmail.com>
2024-04-23 15:28:24 -0700

    second commit

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# It installs and configures Nginx web server with Puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { ' web redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
